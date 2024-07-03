85cec8743838ec6680e61f01ee1399b826e5e380
ArayanPratama <arayanpratama265@gmail.com>
2024-07-03 14:10:26 +0700

    update index.html and index.js

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
+   //// # Ini adalah code satu lagi
+   // var numberOfDrum = document.querySelectorAll(".drum").length;
+   // const listOfSounds = [  
+   // "tom-1.mp3",
+   // "tom-2.mp3",
+   // "tom-3.mp3",
+   // "tom-4.mp3",
+   // "snare.mp3",
+   // "crash.mp3",
+   // "kick-bass.mp3",
+   // ]
+
+   // for (var i = 0; i < numberOfDrum; i++) {
+   //    document.querySelectorAll(".drum")[i].addEventListener("click", (function(i) {
+   //       return function() {
+   //          var audio = new Audio("sounds/"+listOfSounds[i]);
+   //          audio.play();
+
+   //       }
+   //    })(i));
+   // }
+
+
+
    var numberOfDrum = document.querySelectorAll(".drum").length;
-   const listOfSounds = [  
-   "tom-1.mp3",
-   "tom-2.mp3",
-   "tom-3.mp3",
-   "tom-4.mp3",
-   "snare.mp3",
-   "crash.mp3",
-   "kick-bass.mp3",
-   ]
-   
+
    for (var i = 0; i < numberOfDrum; i++) {
-      document.querySelectorAll(".drum")[i].addEventListener("click", (function(i) {
-         return function() {
-            var audio = new Audio("sounds/"+listOfSounds[i]);
-            audio.play();
-         }
-      })(i));
-   }
+   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+      var buttonInnerHTML = this.innerHTML;
+
+      switch (buttonInnerHTML) {
+         case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+         case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+         case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+         case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+         case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+         case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+         case "l":
+            var bass = new Audio("sounds/kick-bass.mp3");
+            bass.play();
+            break;
+      
+         default:
+      }
+
+
+   })
+}
