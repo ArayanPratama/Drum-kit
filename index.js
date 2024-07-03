4238297fb35ba1af28d8f90a62487f16c2a50c57
ArayanPratama <arayanpratama265@gmail.com>
2024-07-03 13:28:46 +0700

    update index.html, index.js and styles.css

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var numberOfDrumButton = document.querySelectorAll(".drum").length;
-
-for (var i = 0; i<numberOfDrumButton; i++){
-   document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    alert("I got clicked");
-   });
-}
-
-
-
-
+   var numberOfDrum = document.querySelectorAll(".drum").length;
+   const listOfSounds = [  
+   "tom-1.mp3",
+   "tom-2.mp3",
+   "tom-3.mp3",
+   "tom-4.mp3",
+   "snare.mp3",
+   "crash.mp3",
+   "kick-bass.mp3",
+   ]
+   
+   for (var i = 0; i < numberOfDrum; i++) {
+      document.querySelectorAll(".drum")[i].addEventListener("click", (function(i) {
+         return function() {
+            var audio = new Audio("sounds/"+listOfSounds[i]);
+            audio.play();
+         }
+      })(i));
+   }
