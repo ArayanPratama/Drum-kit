d46b759fc2c7dce357b47dca92321779bf17b892
ArayanPratama <arayanpratama265@gmail.com>
2024-07-04 14:19:58 +0700

    update index.html and index.js

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-   // // # Ini adalah code satu lagi
-   // var numberOfDrum = document.querySelectorAll(".drum").length;
-   // const listOfSounds = [  
-   // "tom-1.mp3",
-   // "tom-2.mp3",
-   // "tom-3.mp3",
-   // "tom-4.mp3",
-   // "snare.mp3",
-   // "crash.mp3",
-   // "kick-bass.mp3",
-   // ]
+// # Ini adalah code satu lagi
+var numberOfDrum = document.querySelectorAll(".drum").length;
+const listOfSounds = [
+   "tom-1.mp3",
+   "tom-2.mp3",
+   "tom-3.mp3",
+   "tom-4.mp3",
+   "snare.mp3",
+   "crash.mp3",
+      "kick-bass.mp3",
+]
 
-   // for (var i = 0; i < numberOfDrum; i++) {
-   //    document.querySelectorAll(".drum")[i].addEventListener("click", (function(i) {
-   //       return function() {
-   //          var audio = new Audio("sounds/"+listOfSounds[i]);
-   //          audio.play();
+for (var i = 0; i < numberOfDrum; i++) {
+   document.querySelectorAll(".drum")[i].addEventListener("click", (function (i) {
+      return function () {
+         console.log('wwwwww');
+         var audio = new Audio("sounds/" + listOfSounds[i]);
+         audio.play();
+      }
+   })(i));
+}
 
-   //       }
-   //    })(i));
-   // }
 
+for (var i = 0; i < numberOfDrum; i++) {
+   document.querySelectorAll(".drum")[i].addEventListener("S", function () {
+   })
+}
+var buttonInnerHTML = this.innerHTML;
+makeSound(buttonInnerHTML);
+//detecting button press
+var numberOfDrum = document.querySelectorAll(".drum").length;
 
-   
-
-//    for (var i = 0; i < numberOfDrum; i++) {
-//    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-//       var buttonInnerHTML = this.innerHTML;
-//       makeSound(buttonInnerHTML);
-
-//    })
-// }
-
-    //detecting button press
-   var numberOfDrum = document.querySelectorAll(".drum").length;
-
-   document.addEventListener("keypress", function(event){
-      makeSound(event.key);
-   });
-
-  //detecting keyboard press
-function makeSound (key){
+document.addEventListener("keypress", function (e) {
+   makeSound(e.key);
+   console.log(e);
+});
+//detecting keyboard press
+function makeSound(key) {
 
    switch (key) {
       case "w":
@@ -76,8 +72,11 @@ function makeSound (key){
          var bass = new Audio("sounds/kick-bass.mp3");
          bass.play();
          break;
-   
-      default:console.log(buttonInnerHTML);
+
+      default: break;
    }
 }
 
+function anything() {
+   console.log("haiii");
+}
